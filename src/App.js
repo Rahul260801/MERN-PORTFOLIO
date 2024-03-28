da2308757412e3a4b532b17a942085aa944dc6cb
Rahul Jha <jayjha380@gmail.com>
2024-03-29 01:17:19 +0530

    PORTFOLIO

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         </div>
         <div className="footer pb-3 ms-3">
           <Tada>
-            <h4 className="text-center">----------  Made With ❤️ By Rahul Jha   ----------</h4>
+            <h4 className="text-center">-- Made With ❤️ By Rahul Jha  --</h4>
           </Tada>
         </div>
       </div>
